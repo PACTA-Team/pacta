58d2fa0930b2f5118ccfc9fe2b460a52ebb973a2
mowgliph <jelvysc@gmail.com>
2026-04-17 10:41:35 -0400

    feat: add email settings to system_settings table

---
+++ internal/db/migrations/027_email_settings.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- Migration: Email Settings from Database
+-- Date: 2026-04-17
+
+INSERT INTO system_settings (key, value, category) VALUES
+('email_notifications_enabled', 'true', 'email'),
+('email_contract_expiry_enabled', 'true', 'email'),
+('smtp_enabled', 'true', 'email'),
+('brevo_enabled', 'false', 'email'),
+('brevo_api_key', '', 'email');
+
+-- +goose Down
+DELETE FROM system_settings WHERE category = 'email';
